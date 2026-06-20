f468a597c2f3cc54553a58f0258ff57aad08cc7c
Marine-Briet <marinebriet@hotmail.com>
2026-06-20 14:26:04 +0200

    Configure the Sequelize connection to the database and verification tests #8

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ const app = express();
 // On définit le port sur lequel le serveur va écouter
 const PORT = 3000;
 
+// Test :
+const sequelize = require('./config/database');
+
+sequelize.authenticate()
+    .then(() => console.log('✅ Connexion à la base de données réussie.'))
+    .catch((error) => console.error('❌ Erreur de connexion à la base de données :', error));
+
+
+    
 // Route de test, juste pour vérifier que ça fonctionne
 app.get('/', (req, res) => {
   res.send('Le serveur fonctionne!');
